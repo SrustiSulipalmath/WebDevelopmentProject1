031b6641fef1b51c44bfba4e0fe97e62d38665c8
SrustiSulipalmath <97379669+SrustiSulipalmath@users.noreply.github.com>
2022-11-18 16:45:04 +0530

    Add files via upload

---
+++ to do list/main.js
@@ -0,0 +1,11 @@
+let list = document.getElementById("list")
+
+// document.getElementById("add").addEventListener("click", ()=>{console.log("clicked")})
+
+document.getElementById("add").addEventListener("click", ()=>{
+    let listItem = document.createElement("p");
+    listItem.innerHTML = document.getElementById("todo").value
+    list.append(listItem)
+    document.getElementById("todo").value=""
+    listItem.addEventListener("click", ()=>listItem.remove())
+})
